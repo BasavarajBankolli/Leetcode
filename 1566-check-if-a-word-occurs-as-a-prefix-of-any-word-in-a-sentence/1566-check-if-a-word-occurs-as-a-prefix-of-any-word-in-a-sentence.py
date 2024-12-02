89eaae94c6ecd956936d228eb8df6a4728595956
Basavaraj Bankolli <121506529+BasavarajBankolli@users.noreply.github.com>
2024-12-02 19:01:43 +0530

    Time: 0 ms (100%), Space: 17.2 MB (5.67%) - LeetHub

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
@@ -6,7 +6,18 @@ class Solution:
 
         for i in range(n):
 
-            if len(words[i]) >= m and words[i][0:m] == sw:
-                return i + 1
+            if len(words[i]) >= m:
+                cnt = 0
+                j = 0
+                while j < m:
+                    if words[i][j] == sw[cnt]:
+                        cnt += 1
+                    else:
+                        break
+                    j += 1
+                
+                if cnt == m:
+                    return i + 1
+                
 
         return -1
