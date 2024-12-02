d405ee1593368b9294a2c8be2b58c5e941f23261
Basavaraj Bankolli <121506529+BasavarajBankolli@users.noreply.github.com>
2024-12-02 19:04:24 +0530

    Time: 0 ms (100%), Space: 17.1 MB (7.12%) - LeetHub

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
@@ -1,23 +1,9 @@
 class Solution:
     def isPrefixOfWord(self, s: str,sw: str) -> int:
         words = s.split()
-        n = len(words)
-        m = len(sw)
-
-        for i in range(n):
-
-            if len(words[i]) >= m:
-                cnt = 0
-                j = 0
-                while j < m:
-                    if words[i][j] == sw[cnt]:
-                        cnt += 1
-                    else:
-                        break
-                    j += 1
-                
-                if cnt == m:
-                    return i + 1
-                
+        
+        for i,word in enumerate(words):
+            if word.startswith(sw):
+                return i + 1
 
         return -1
