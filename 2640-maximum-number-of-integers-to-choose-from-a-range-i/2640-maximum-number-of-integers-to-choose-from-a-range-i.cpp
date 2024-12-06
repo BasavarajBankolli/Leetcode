c3113cc5200c515ccb72ba4abcff931db3695f3d
Basavaraj Bankolli <121506529+BasavarajBankolli@users.noreply.github.com>
2024-12-06 20:18:23 +0530

    Time: 4 ms (99.24%), Space: 122.5 MB (74.57%) - LeetHub

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int maxCount(vector<int>& banned, int n, int maxSum) {
-        
-        unordered_set<int> set(banned.begin(), banned.end());
+
+        vector<int> vec(n + 1, 0);
+        for (const auto& v: banned) {
+            if (v <= n) vec[v] = 1;
+        }
+
+
         int cnt = 0;
         long long sum = 0;
 
         for (int i = 1; i < n + 1; i ++) {
             // set.find(i) == set.end() means that i is not there in set..
-            if (set.find(i) == set.end() && (sum + i) <= maxSum) {
+            if (!(vec[i]) && (sum + i) <= maxSum) {
                 cnt++;
                 sum += i;
             }
