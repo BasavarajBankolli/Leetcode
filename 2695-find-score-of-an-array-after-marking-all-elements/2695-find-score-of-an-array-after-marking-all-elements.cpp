2d614a910634724af27abb40d74712f952001e45
Basavaraj Bankolli <121506529+BasavarajBankolli@users.noreply.github.com>
2024-12-13 20:10:15 +0530

    Time: 47 ms (98.56%), Space: 99.5 MB (85.95%) - LeetHub

---
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    long long findScore(vector<int>& nums) {
+        const int N = 100000, n = nums.size();
+        bitset<N> mark = 0;
+        vector<pair<int, int>> nIdx(n);
+
+        for(int i = 0; i < n; i++)
+            nIdx[i] = {nums[i], i};
+
+        sort(nIdx.begin(), nIdx.end());
+
+        long long sum = 0;
+        for(auto& [x, i]: nIdx){
+            if (mark[i]) continue;
+            sum += x;
+
+            mark[i] = 1;
+            if (i > 0) mark[i-1] = 1;
+            if (i < n-1) mark[i+1] = 1;
+        }
+
+        return sum;
+    }
+};
