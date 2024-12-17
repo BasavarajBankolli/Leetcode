59e76ec2dfa286df79d1e859e2ebf4efc44ffb15
Basavaraj Bankolli <121506529+BasavarajBankolli@users.noreply.github.com>
2024-12-17 21:54:32 +0530

    Time: 108 ms (99.28%), Space: 19.8 MB (6.99%) - LeetHub

--- 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.py
+++ 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.py
@@ -1,30 +1,27 @@
 class Solution:
     def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
-        s = list(s)
-        result = []
-        freq = 0
-        pointer = 0
-
-        s.sort(reverse=True)
-
-        for i in range(len(s)):
-            if result and result[-1] == s[i]:
-                if freq < repeatLimit:
-                    result.append(s[i])
-                    freq += 1
-                else:
-                    pointer = max(pointer, i + 1)
-                    while pointer < len(s) and s[pointer] == s[i]:
-                        pointer += 1
-
-                    if pointer < len(s):
-                        result.append(s[pointer])
-                        s[i], s[pointer] = s[pointer], s[i]
-                        freq = 1
-                    else:
-                        break
+        chars = Counter(s)
+        sorted_chars = sorted(chars.items(), reverse=True)
+        
+        ans = []
+        
+        while sorted_chars:
+            char, freq = sorted_chars[0]
+            if freq <= repeatLimit:
+                ans.append(char * freq)
+                sorted_chars.pop(0)
             else:
-                result.append(s[i])
-                freq = 1
-
-        return ''.join(result)
+                ans.append(char * repeatLimit)
+                sorted_chars[0] = (char, freq - repeatLimit)
+                
+                if len(sorted_chars) > 1:
+                    next_char, next_freq = sorted_chars[1]
+                    ans.append(next_char)
+                    if next_freq == 1:
+                        sorted_chars.pop(1)
+                    else:
+                        sorted_chars[1] = (next_char, next_freq - 1)
+                else:
+                    break
+        
+        return ''.join(ans)
