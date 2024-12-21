053f611d86768536ecf7baffb6381bfcacfd9581
Basavaraj Bankolli <121506529+BasavarajBankolli@users.noreply.github.com>
2024-12-21 15:04:09 +0530

    Time: 0 ms (100%), Space: 10.6 MB (62.64%) - LeetHub

---
+++ 3540-hash-divided-string/3540-hash-divided-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string stringHash(const string& s, int k) {
+        string res = "";
+        int n = s.size();
+
+        for (int i = 0; i < n; i += k) {
+            int j = i + k - 1;
+            int cnt = 0;
+
+            while (j >= i) {
+                cnt += s[j] - 'a';
+                j--;
+            }
+
+            cnt %= 26;
+            res += (char)('a' + cnt);
+        }
+
+        return res;
+    }
+
+};
