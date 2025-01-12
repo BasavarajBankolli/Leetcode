9fcff86e96d18d7026ff008b411c6dd2cc307eac
pb <121506529+BasavarajBankolli@users.noreply.github.com>
2025-01-12 19:47:11 +0530

    Time: 0 ms (100%), Space: 18.3 MB (100%) - LeetHub

---
+++ 3708-zigzag-grid-traversal-with-skip/3708-zigzag-grid-traversal-with-skip.py
@@ -0,0 +1,8 @@
+class Solution:
+    def zigzagTraversal(self, grid: List[List[int]]) -> List[int]:
+        res = []
+        
+        for i, r in enumerate(grid):
+            res += r[::-1] if i % 2 else r
+
+        return res[::2]
