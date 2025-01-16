676bb9c6a7eac6429f95ba1c84c66f9683fbf276
pb <121506529+BasavarajBankolli@users.noreply.github.com>
2025-01-16 11:04:24 +0530

    Time: 0 ms (100%), Space: 28 MB (87.85%) - LeetHub

--- 3691-minimum-operations-to-make-columns-strictly-increasing/3691-minimum-operations-to-make-columns-strictly-increasing.cpp
+++ 3691-minimum-operations-to-make-columns-strictly-increasing/3691-minimum-operations-to-make-columns-strictly-increasing.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int minimumOperations(vector<vector<int>> &grid) {
+    int minimumOperations(vector<vector<int>>& grid) {
+        
         const int n = grid[0].size();
         const int m = grid.size();
-        int res = 0;
-
+        int res = 0, temp;
         for (int i = 0; i < n; i++) {
             for (int j = 1; j < m; j++) {
-                int diff = grid[j - 1][i] - grid[j][i] + 1;
-                if (diff > 0) {
-                    res += diff;
-                    grid[j][i] += diff;
+                temp = (grid[j-1][i] - grid[j][i]) + 1;
+                if (temp > 0){
+                    res += temp;
+                    grid[j][i] += temp;
                 }
             }
         }
         return res;
     }
-
 };
