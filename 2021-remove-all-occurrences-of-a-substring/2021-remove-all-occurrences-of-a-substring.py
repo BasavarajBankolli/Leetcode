c00994caab3d52b61541dd49b0c10bfbdf831ca9
pb <121506529+BasavarajBankolli@users.noreply.github.com>
2025-02-11 11:31:24 +0530

    Time: 15 ms (24.95%), Space: 18 MB (32.79%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.py
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.py
@@ -1,14 +1,12 @@
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
         res = []
-        part = list(part)
         pn = len(part)
 
         for c in s:
             res.append(c)
-            
-            while len(res) >= pn and res[len(res) - pn:] == part:
-                for i in range(pn):
-                    res.pop()
+
+            while len(res) >= pn and ''.join(res[-pn:]) == part:
+                del res[-pn:]
 
         return "".join(res)
