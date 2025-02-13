e5a0bccdfd032c130c30a20f0de3c951aacd8659
pb <121506529+BasavarajBankolli@users.noreply.github.com>
2025-02-13 14:53:20 +0530

    Time: 76 ms (87.8%), Space: 85.4 MB (74.02%) - LeetHub

--- 2310-minimum-operations-to-halve-array-sum/2310-minimum-operations-to-halve-array-sum.cpp
+++ 2310-minimum-operations-to-halve-array-sum/2310-minimum-operations-to-halve-array-sum.cpp
@@ -3,20 +3,16 @@ public:
     int halveArray(vector<int>& nums) {
         double sum = accumulate(nums.begin(), nums.end(), 0.0), half = sum/2;
         
-        priority_queue<double> pq;
-        
-        for (int a: nums) {
-            pq.push((double)a);
-        }
+        priority_queue<double> pq(nums.begin(), nums.end());
 
         int cnt = 0; double tmp;
         while (sum > half){
             cnt ++;
-            tmp = pq.top();
+            tmp = pq.top();  // access mx value
             tmp /= 2;
             sum -= tmp;
-            pq.pop();
-            pq.push(tmp);
+            pq.pop();       // pop that val
+            pq.push(tmp);   // again push to pq for further check..
         }  
 
         return cnt;
