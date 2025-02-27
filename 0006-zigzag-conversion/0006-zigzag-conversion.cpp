5ecad9770a0fb3f0755b225395692231d38f4c81
pb <121506529+BasavarajBankolli@users.noreply.github.com>
2025-02-27 12:42:57 +0530

    Time: 4 ms (65.38%), Space: 14.2 MB (39.78%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -8,19 +8,20 @@ public:
         vector<string>mp(numRows, "");
 
         int i = 0, r = 0;
+        
         while(i < s.size()){
             while (i < s.size() && r < numRows){
-                mp[r++].push_back(s[i++]);
+                mp[r++] += s[i++];
             }
             r -= 2;
             while(i < s.size() && r > -1){
-                mp[r--].push_back(s[i++]);
+                mp[r--] += s[i++];
             }
             r += 2;
         }
 
-        string res = "";   
-        for (string st: mp){
+        string res;   
+        for (string &st: mp){
             res += st;
         }
 
