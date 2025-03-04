fb05748f4a72e919b98b4f7d362c0ffed69c635c
pb <121506529+BasavarajBankolli@users.noreply.github.com>
2025-03-04 22:08:44 +0530

    Time: 0 ms (100%), Space: 7.7 MB (91.08%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     bool checkPowersOfThree(int n) {
-        map<int, int> degree = {{16, 43046721}, {15, 14348907}, {14, 4782969}, 
-         {13, 1594323}, {12, 531441},{11, 177147}, {10, 59049}, {9, 19683}, {8, 6561},
-         {7, 2187}, {6, 729}, {5, 243}, {4, 81}, {3, 27}, {2, 9}, {1, 3}, {0, 1}
-        };
-
-        for (auto it = degree.rbegin(); it != degree.rend(); ++it) {  
-            if (it->second <= n) {
-                n -= it->second;
-            }
+        while (n) {
+            if (n % 3 == 2) return 0;
+            n /= 3;
         }
 
         return n == 0;
-
     }
 };
