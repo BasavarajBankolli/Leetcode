1d3d07259d1d08a78c5b7d8dad405e111f0f535b
pb <121506529+BasavarajBankolli@users.noreply.github.com>
2025-03-27 01:09:05 +0530

    Time: 3 ms (69.08%), Space: 17.8 MB (64.91%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -2,8 +2,8 @@ class Solution:
     def wordBreak(self, s: str, words: List[str]) -> bool:
         bp = [False]*(len(s) + 1)
         
-        bp[-1] = True
-        
+        bp[-1] = True # bottom_up approach
+
         for i in range(len(s)-1, -1, -1):
             
             for w in words:
