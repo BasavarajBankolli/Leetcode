21a86ea2ac3ced9c73291bd35d4c0ab53e421e1f
pb <121506529+BasavarajBankolli@users.noreply.github.com>
2025-04-23 15:01:29 +0530

    Time: 23 ms (81.75%), Space: 17.7 MB (90.25%) - LeetHub

--- 1500-count-largest-group/1500-count-largest-group.py
+++ 1500-count-largest-group/1500-count-largest-group.py
@@ -1,5 +1,7 @@
 class Solution:
     def digitsum(self,n):
+        if n < 10:
+            return n
         res = 0
         while n:
             res += n % 10
@@ -7,17 +9,16 @@ class Solution:
         return res
 
     def countLargestGroup(self, n: int) -> int:
-        d = defaultdict(list)
+        d = defaultdict(int)
         
-        mx = 0
         for v in range(1,n+1):
             som = self.digitsum(v)
-            d[som].append(v)
-            mx = max(len(d[som]), mx)
-
+            d[som] += 1
+        
+        mx = max(d.values())
         resCnt = 0
-        for arr in d.values():
-            if len(arr) == mx:
+        for v in d.values():
+            if v == mx:
                 resCnt += 1
 
         return resCnt
