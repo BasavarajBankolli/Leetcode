dcb24f60a26520efb5d47fae8c778b5ac7022db0
pb <121506529+BasavarajBankolli@users.noreply.github.com>
2025-05-06 00:28:58 +0530

    Time: 43 ms (89.15%), Space: 18 MB (56.85%) - LeetHub

--- 2487-optimal-partition-of-string/2487-optimal-partition-of-string.py
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.py
@@ -1,16 +1,13 @@
 class Solution:
     def partitionString(self, s: str) -> int:
-        freq = [0]*26
-
-        cnt = 0
-        for i in range(len(s)):
-            if freq[ord(s[i]) - ord('a')]:
+        cnt = 1
+        seen = set()
+        
+        for c in s:
+            if c in seen:
                 cnt += 1
-                freq = [0] * 26
-
-            freq[ord(s[i]) - ord('a')] = 1
-
-        if freq.count(1):
-            return cnt + 1
+                seen = {c}
+            else:
+                seen.add(c)
 
         return cnt
