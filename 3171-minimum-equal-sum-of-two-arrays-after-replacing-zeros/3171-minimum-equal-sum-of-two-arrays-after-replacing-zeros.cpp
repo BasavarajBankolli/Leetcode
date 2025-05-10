c73d21a516075aca06125fb7abda0b97568e4ccb
pb <121506529+BasavarajBankolli@users.noreply.github.com>
2025-05-10 13:59:49 +0530

    Time: 120 ms (87.46%), Space: 135.5 MB (50.17%) - LeetHub

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -12,21 +12,11 @@ public:
             if(v == 0) cnt2++;
             sum2 += v;
         }
-
-        if (cnt1 == 0 && cnt2 == 0) {
-            if(sum1 != sum2) return -1;
-            return sum1;
-        }
         
         sum1 += cnt1;
         sum2 += cnt2;
+        if (cnt1 == 0 && sum1 < sum2 || cnt2 == 0 && sum2 < sum1) return -1;
 
-        if (sum1 <= sum2 && cnt1)return sum2;
-        else if(sum2 <= sum1 && cnt2) return sum1;
-        else{
-            if (cnt2 == 0 && sum2 >= sum1) return sum2;
-            else if(cnt1 == 0 && sum1 >= sum2) return sum1;
-        }
-        return -1;
+        return max(sum1, sum2);
     }
 };
